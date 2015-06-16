97be67eca5050d2f8b5ed4ccb656f9f05ce93962
Chris Brody <chrisbrody@codifyacademy.com>
2015-06-16 15:38:52 -0400

    thrid commit

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -4,6 +4,7 @@ app.controller('ProfileCtrl', function ($scope, $routeParams, Profile) {
   var uid = $routeParams.userId;
 
   $scope.profile = Profile.get(uid);
+  
   Profile.getPosts(uid).then(function(posts) {
     $scope.posts = posts;
   });
--- app/scripts/services/profile.js
+++ app/scripts/services/profile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.factory('Profile', function ($window, FIREBASE_URL, $firebase, Post, $q) {
+
   var ref = new $window.Firebase(FIREBASE_URL);
 
   var profile = {
